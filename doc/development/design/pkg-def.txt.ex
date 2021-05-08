bc84e318b105a73521e0e8e3fff7250a7a30fcf4
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-05-08 17:40:01 -0400

    More white space fixes

--- doc/development/design/pkg-def.txt.ex
+++ doc/development/design/pkg-def.txt.ex
@@ -3,11 +3,13 @@ requires env('c');
 def c_library('gtk3') {
   header_file('gtk/gtk.h'), type => main_header;
 }
+ 
 def apt('libgtk-3-0') {
   # version '*';  # implied
   requires dist('debian') | dist('ubuntu');
   provides shared_library;
 }
+ 
 def apt('libgtk-3.0-dev') {
   requires dist('debian') | dist('ubuntu');
   provides env('c')->headers;
